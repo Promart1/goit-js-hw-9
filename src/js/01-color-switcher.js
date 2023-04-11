ce493342d96c779b4015e4cfe92b7dc22354f5ac
Promart1 <marta.batih@gmail.com>
2023-04-11 22:16:45 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body')
 
+
+startBtn.addEventListener("click", () => {
+    startId = setInterval(() => {
+        changeColor();
+       startBtn.disabled = true;
+        console.log(changeColor);
+    }, 1000);
+
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(startId);
+    startBtn.disabled = false;
+})
+
+function changeColor() {
+        const color = getRandomHexColor();
+        body.style.background = color;
+      };
+
+function changeColor() {
+    const color = getRandomHexColor();
+    body.style.background = color;
+  }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
