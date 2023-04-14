8e267669dccd275b16fb84c819357fbdc06f4c78
Promart1 <117581510+Promart1@users.noreply.github.com>
2023-04-14 20:38:34 +0000

    Deploying to gh-pages from @ Promart1/goit-js-hw-9@0872dddb1d8d07404cbab70b81f91552bddca64e 🚀

---
+++ 01-color-switcher.60160548.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function o(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.background=t}t.addEventListener("click",(function(){startId=setInterval((function(){o(),t.disabled=!0,console.log(o)}),1e3)})),e.addEventListener("click",(function(){clearInterval(startId),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.60160548.js.map
